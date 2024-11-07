4b567db762a05b8ac7c4bae42db050b02e766412
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-11-06 20:36:16 -0500

    Made Improvements

--- validlinuxtextfilechecker.go
+++ validlinuxtextfilechecker.go
@@ -11,11 +11,36 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	var hasNonASCIIPrintableCharacters bool = false
+	var hasLineFeed bool = false
+	var hasHorizontalTab bool = false
+
 	for _, v := range data {
-		if ((v < 0x20) || (v > 0x7e)) && v != 0xa && v != 0x9 {
-			fmt.Println("Fail")
-			os.Exit(1)
+		if v == 0xa {
+			hasLineFeed = true
+		} else if v == 0x9 {
+			hasHorizontalTab = true
+		} else if (v < 0x20) || (v > 0x7e) {
+			hasNonASCIIPrintableCharacters = true
 		}
 	}
-	fmt.Println("Pass")
+
+	if hasLineFeed {
+		fmt.Println("Line Feed: YES")
+	} else {
+		fmt.Println("Line Feed: NO")
+	}
+
+	if hasHorizontalTab {
+		fmt.Println("Horizontal Tab: YES")
+	} else {
+		fmt.Println("Horizontal Tab: NO")
+	}
+
+	if hasNonASCIIPrintableCharacters {
+		fmt.Println("Non ASCII Printable Characters: YES")
+	} else {
+		fmt.Println("Non ASCII Printable Characters: NO")
+	}
 }
