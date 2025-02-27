dce42cce160c6c7ac90a498e3ec503d4adaf5063
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2025-02-27 16:14:41 -0500

    Added line tracker feature

--- validlinuxtextfilechecker.go
+++ validlinuxtextfilechecker.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -16,12 +17,19 @@ func main() {
 	var hasLineFeed bool = false
 	var hasHorizontalTab bool = false
 
+	var lineFeedCount int = 0
+	var firstNonASCIIPrintableCharacterLine int = 0
+
 	for _, v := range data {
 		if v == 0xa {
 			hasLineFeed = true
+			lineFeedCount++
 		} else if v == 0x9 {
 			hasHorizontalTab = true
 		} else if (v < 0x20) || (v > 0x7e) {
+			if !hasNonASCIIPrintableCharacters {
+				firstNonASCIIPrintableCharacterLine = lineFeedCount
+			}
 			hasNonASCIIPrintableCharacters = true
 		}
 	}
@@ -39,7 +47,8 @@ func main() {
 	}
 
 	if hasNonASCIIPrintableCharacters {
-		fmt.Println("Non ASCII Printable Characters: YES")
+		fmt.Println("Non ASCII Printable Characters: YES" + " " + "(" + "Line" + " " +
+			strconv.FormatInt(int64(firstNonASCIIPrintableCharacterLine+1), 10) + ")")
 	} else {
 		fmt.Println("Non ASCII Printable Characters: NO")
 	}
